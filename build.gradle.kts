443f01063cd543e146675535c562f9ef012a2c84
kaiyu.shan <kaiyu.shan@outlook.com>
2025-01-14 15:13:20 +0800

    refactor(mumu): 优化版本定义

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.changelog.date
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import java.time.OffsetDateTime
 import java.time.ZoneOffset
+import java.time.ZonedDateTime
 import java.time.format.DateTimeFormatter
 
 plugins {
@@ -17,10 +18,24 @@ fun properties(key: String) = providers.gradleProperty(key)
 val gitHash = providers.exec {
     commandLine("git", "rev-parse", "--short", "HEAD")
 }.standardOutput.asText.get().trim()
+val suffixes = listOf("-alpha", "-beta", "-snapshot", "-dev", "-test", "-pre")
+val now: ZonedDateTime = ZonedDateTime.now(ZoneOffset.UTC)
+
+@Suppress("SpellCheckingInspection")
+val formatter: DateTimeFormatter = DateTimeFormatter.ofPattern("yyyyMMddHHmmssXXX")
+val formattedTime: String = now.format(formatter)
+fun endsWithAny(input: String, suffixes: List<String>): Boolean {
+    return suffixes.any { input.endsWith(it, ignoreCase = true) }
+}
 
 group = findProperty("group")!! as String
 val versionString = findProperty("version")!! as String
-version = if (versionString.contains("-")) "$versionString-$gitHash" else versionString
+version =
+    if (endsWithAny(
+            versionString,
+            suffixes
+        )
+    ) "$versionString-$gitHash-$formattedTime" else versionString
 
 repositories {
     mavenCentral()
